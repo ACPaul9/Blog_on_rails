14414d068bcfa76cb82d5ff86aad3ef05837aca5
ACPaul9 <a.yudin@sftpro.ru>
2022-09-16 09:03:48 +0300

    fix filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: [:show]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :set_current_user_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -63,7 +63,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-  def set_current_user_event
+  def set_current_user_post
     @post = current_user.posts.find(params[:id])
   end
 end
